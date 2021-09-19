ce1429a9258a6799c62a23136f0e22b18f6e3587
Robert Arifulin <robertarif2015@gmail.com>
2021-09-19 11:54:25 +0300

    subprocess test

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import importlib
 import os
-import inspect
+import sys, inspect
+import subprocess
+
 
 
 def bots_import():
     """Импортирует модули с ботами в словарь."""
     all_bots = {}
-    new_bots = [i[:-3] for i in os.listdir() if '.py' in i and i != 'main.py']
+    new_bots = [i[:-3] for i in os.listdir() if '.py' in i and i != 'main.py' and i != 'gametest.py']
     for bot in new_bots:
         all_bots.update({bot: importlib.import_module(bot)})
     return all_bots
@@ -21,4 +23,7 @@ bot1 = bots_init('testbot1', '123')
 bot2 = bots_init('testbot2', '123')
 print(bot1.play())
 print(bot2.play())
-# print(inspect.getmembers(all_bots['testbot1'], inspect.isclass)[0][1])
+
+res = subprocess.run([sys.executable, 'gametest.py'], input=b'1 2', capture_output=True)
+print(str(res.stdout))
+print(res.stderr)
